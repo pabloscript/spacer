329fbe560641d95ef9ca105fbd31ebd7160a67f4
pabloscript <pe.kozicki@gmail.com>
2019-02-04 00:33:10 +0100

    Add basic main page view

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="searchWrapper">
+    <input
+      id="search"
+      type="text"
+      name="search"
+      v-model="searchValue"
+      @input="handleInput"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchInput',
+};
+</script>
+
+<style lang="scss" scoped>
+
+  .searchWrapper {
+    margin-top: 50px;
+    display: flex;
+    flex-direction: column;
+    width: 250px;
+
+    input {
+      height: 30px;
+      border: 0;
+      background: none;
+      border-bottom: 1px solid #fff;
+    }
+  }
+
+</style>
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,31 +1,24 @@
 <template>
   <div class="wrapper">
-    <div class="search">
-      <label for="search">Search</label>
-      <input
-        id="search"
-        type="text"
-        name="search"
-        v-model="searchValue"
-        @input="handleInput"
-      />
-      <ul>
-        <li v-for="item in results" :key="item.data[0].nasa_id">
-          <p>{{ item.data[0].description}}</p>
-        </li>
-      </ul>
-    </div>
+    <Claim />
+    <SearchInput />
   </div>
 </template>
 
 <script>
 import axios from 'axios';
 import debounce from 'lodash.debounce';
+import Claim from '@/components/Claim.vue';
+import SearchInput from '@/components/SearchInput.vue';
 
 const API = 'https://images-api.nasa.gov/search';
 
 export default {
   name: 'Search',
+  components: {
+    Claim,
+    SearchInput,
+  },
   data() {
     return {
       searchValue: '',
@@ -48,28 +41,18 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
   .wrapper {
     display: flex;
     flex-direction: column;
     align-items: center;
+    justify-content: center;
     margin: 0;
     padding: 30px;
     width: 100%;
+    height: 100vh;
+    background-size: cover;
+    background: url('../assets/heroimage.jpg') no-repeat 80% 100%;
   }
 
-  .search {
-    display: flex;
-    flex-direction: column;
-    width: 250px;
-
-    label {
-      font-family: Montserrat, sans-serif;
-    }
-
-    input {
-      height: 30px;
-      border: 0;
-      border-bottom: 1px solid black;
-    }
-  }
 </style>
