783a04ac0807f2e5d594fc81b4823bc2a9beae0e
pabloscript <pe.kozicki@gmail.com>
2019-02-04 20:55:10 +0100

    Add search animations and after-search view

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
 <template>
-  <div class="wrapper">
-    <HeroImage />
-    <Claim />
-    <SearchInput v-model="searchValue" @input="handleInput" />
+  <div :class="[{ flexStart: step === 1 }, 'wrapper']">
+    <transition name="slide">
+      <img src="./assets/logo.svg" alt="spacer logo" class="logo" v-if="step === 1">
+    </transition>
+
+    <transition name="fade">
+      <HeroImage v-if="step === 0"/>
+    </transition>
+
+    <Claim v-if="step === 0"/>
+
+    <SearchInput v-model="searchValue" @input="handleInput" :dark="step === 1"/>
+
+    <div class="results" v-if="results && !loading && step === 1">
+      <Item v-for="item in results" :item="item" :key="item.data[0].nasa_id" />
+    </div>
   </div>
 </template>
 
@@ -12,6 +24,7 @@ import debounce from 'lodash.debounce';
 import Claim from '@/components/Claim.vue';
 import SearchInput from '@/components/SearchInput.vue';
 import HeroImage from '@/components/HeroImage.vue';
+import Item from '@/components/Item.vue';
 
 const API = 'https://images-api.nasa.gov/search';
 
@@ -21,9 +34,12 @@ export default {
     HeroImage,
     Claim,
     SearchInput,
+    Item,
   },
   data() {
     return {
+      loading: false,
+      step: 0,
       searchValue: '',
       results: [],
     };
@@ -31,9 +47,12 @@ export default {
   methods: {
     // eslint-disable-next-line
     handleInput: debounce(function () {
+      this.loading = true;
       axios.get(`${API}?q=${this.searchValue}&media_type=image`)
         .then((response) => {
           this.results = response.data.collection.items;
+          this.loading = false;
+          this.step = 1;
         })
         .catch((error) => {
           console.error('Error', error);
@@ -59,15 +78,55 @@ export default {
     padding: 0;
   }
 
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .5s ease;
+  }
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
+  }
+
+  .slide-enter-active, .slide-leave-active {
+    transition: margin-top .5s ease;
+  }
+  .slide-enter, .slide-leave-to {
+    margin-top: -50px;
+  }
+
   .wrapper {
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
+    position: relative;
     margin: 0;
     padding: 30px;
     width: 100%;
     min-height: 100vh;
+  }
+
+  .flexStart {
+    justify-content: flex-start;
+  }
+
+  .logo {
+    position: absolute;
+    top: 30px;
+  }
+
+  .results {
+    margin-top: 50px;
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-gap: 20px;
+
+    @media (min-width: 500px) {
+      grid-template-columns: 1fr 1fr;
+    }
+
+    @media (min-width: 768px) {
+      width: 90%;
+      grid-template-columns: 1fr 1fr 1fr;
+    }
   }
 
 </style>
---
+++ src/components/Item.vue
@@ -0,0 +1,45 @@
+<template>
+  <div :style="style" class="item"></div>
+</template>
+
+<script>
+export default {
+  name: 'Item',
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      photo: this.item.links[0].href,
+      title: this.item.data[0].title,
+    };
+  },
+  computed: {
+    style() {
+      return `background-image: url("${this.photo}")`;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+  .item {
+    width: 80vw;
+    height: 250px;
+    background-size: cover;
+    background: no-repeat 50%;
+
+    @media (min-width: 500px) {
+      width: 40vw;
+    }
+
+    @media (min-width: 768px) {
+      width: 25vw;
+    }
+  }
+
+</style>
